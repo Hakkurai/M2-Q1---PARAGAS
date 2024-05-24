6ad796861e2eed8fc52917844cf16c415f51633a
Hakkurai <gparagas2003@gmail.com>
2024-05-24 15:48:22 +0800

    final update

--- scripts/GameScene.js
+++ scripts/GameScene.js
@@ -1,6 +1,9 @@
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
+        this.score = 0;
+        this.meatColors = ['0xff0000', '0xffa500', '0xffff00', '0x008000', '0x0000ff', '0x4b0082', '0xee82ee'];
+        this.meatColorIndex = 0;
     }
 
     preload() {
@@ -14,17 +17,17 @@ class GameScene extends Phaser.Scene {
     create() {
         this.add.image(600, 300, 'background');
 
-        platforms = this.physics.add.staticGroup();
-        platforms.create(190, 570, 'ground').setScale(1).refreshBody();
-        platforms.create(610, 400, 'ground');
-        platforms.create(140, 250, 'ground');
-        platforms.create(750, 220, 'ground');
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(190, 570, 'ground').setScale(1).refreshBody();
+        this.platforms.create(610, 400, 'ground');
+        this.platforms.create(140, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
 
-        player = this.physics.add.sprite(100, 350, 'dino');
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-        this.physics.add.collider(player, platforms);
-        player.body.setGravityY(300);
+        this.player = this.physics.add.sprite(100, 350, 'dino');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, this.platforms);
+        this.player.body.setGravityY(300);
 
         this.anims.create({
             key: 'left',
@@ -46,88 +49,88 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         });
 
-        meats = this.physics.add.group({
+        this.meats = this.physics.add.group({
             key: 'meat',
             repeat: 11,
             setXY: { x: 12, y: 0, stepX: 70 }
         });
         
-        meats.children.iterate(function (child) {
+        this.meats.children.iterate(function (child) {
             child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
         });
 
-        this.physics.add.collider(meats, platforms);
-        this.physics.add.overlap(player, meats, collectMeat, null, this);
+        this.physics.add.collider(this.meats, this.platforms);
+        this.physics.add.overlap(this.player, this.meats, this.collectMeat, null, this);
 
-        scoreText = this.add.text(16, 16, 'Meat Collected: 0', { fontSize: '20px', fill: '#000' }).setOrigin(-2.4, 0);
+        this.scoreText = this.add.text(16, 16, 'Meat Collected: 0', { fontSize: '20px', fill: '#000' }).setOrigin(-2.4, 0);
 
-        meteors = this.physics.add.group();
-        this.physics.add.collider(meteors, platforms);
-        this.physics.add.collider(player, meteors, hitMeteor, null, this);
+        this.meteors = this.physics.add.group();
+        this.physics.add.collider(this.meteors, this.platforms);
+        this.physics.add.collider(this.player, this.meteors, this.hitMeteor, null, this);
+
+        this.cursors = this.input.keyboard.createCursorKeys(); // Move this to create()
     }
 
     update() {
-        const cursors = this.input.keyboard.createCursorKeys();
-
-        if (cursors.left.isDown) {
-            player.setVelocityX(-160);
-            player.anims.play('left', true);
-        } else if (cursors.right.isDown) {
-            player.setVelocityX(160);
-            player.anims.play('right', true);
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
         } else {
-            player.setVelocityX(0);
-            player.anims.play('turn');
+            this.player.setVelocityX(0);
+            this.player.anims.play('turn');
         }
 
-        if (cursors.up.isDown && player.body.touching.down) {
-            player.setVelocityY(-500);
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-500);
         }
     }
-}
 
-function collectMeat(player, meat) {
-    meat.disableBody(true, true);
+    collectMeat(player, meat) {
+        meat.disableBody(true, true);
 
-    score += 10;
-    scoreText.setText('Meat Collected: ' + score);
+        this.score += 10;
+        this.scoreText.setText('Meat Collected: ' + this.score);
 
-    if (++meatColorIndex >= meatColors.length) {
-        meatColorIndex = 0;
+        if (++this.meatColorIndex >= this.meatColors.length) {
+            this.meatColorIndex = 0;
+        }
+        this.player.setTint(parseInt(this.meatColors[this.meatColorIndex], 16));
+
+        if (this.score % 50 === 0) {
+            this.player.setScale(this.player.scaleX + 0.1, this.player.scaleY + 0.1);
+        }
+
+        if (this.meats.countActive(true) === 0) {
+            this.meats.children.iterate(function (child) {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            let x = Phaser.Math.Between(0, 800);
+            let newMeat = this.meats.create(x, 0, 'meat');
+            newMeat.setBounce(Phaser.Math.FloatBetween(0.4, 0.8));
+        }
+
+        if (this.score % 120 === 0) {
+            let x = Phaser.Math.Between(0, 800);
+            let meteor = this.meteors.create(x, 0, 'meteor');
+            meteor.setBounce(1);
+            meteor.setCollideWorldBounds(true);
+            meteor.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
     }
-    player.setTint(parseInt(meatColors[meatColorIndex], 16));
 
-    if (score % 50 === 0) {
-        player.setScale(player.scaleX + 0.1, player.scaleY + 0.1);
+    hitMeteor(player, meteor) {
+        this.physics.pause();
+
+        this.player.setTint(0xff0000);
+
+        this.player.anims.play('turn');
+
+        this.add.text(400, 300, 'Game Over', { fontSize: '48px', fill: '#ff0000' }).setOrigin(0.5);
     }
-
-    if (meats.countActive(true) === 0) {
-        meats.children.iterate(function (child) {
-            child.enableBody(true, child.x, 0, true, true);
-        });
-
-        var x = Phaser.Math.Between(0, 800);
-        var meat = meats.create(x, 0, 'meat');
-        meat.setBounce(Phaser.Math.FloatBetween(0.4, 0.8));
-    }
-
-    if (score % 120 === 0) {
-        var x = Phaser.Math.Between(0, 800);
-        var meteor = meteors.create(x, 0, 'meteor');
-        meteor.setBounce(1);
-        meteor.setCollideWorldBounds(true);
-        meteor.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    }
-}
-
-function hitMeteor(player, meteor) {
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    this.add.text(400, 300, 'Game Over', { fontSize: '48px', fill: '#ff0000' }).setOrigin(0.5);
 }
 
 export default GameScene;
